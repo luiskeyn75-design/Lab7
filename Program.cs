041247d2835be5837d9b709916fa7cb15925e29e
DN <luiskeyn75@gmail.com>
2026-02-18 20:49:35 +0200

    Task3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab7Task3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<double, double> discountCalculator = (price) => price * 0.95;
+            discountCalculator += (price) => price * 0.90;
+            discountCalculator += (price) => price - 100;
+
+            double finalprice = 1000;
+            var delegates = discountCalculator.GetInvocationList();
+            foreach(var del in delegates)
+            {
+                var method = (Func<double, double>)del;
+                finalprice = method(finalprice);
+            }
+            Console.WriteLine(finalprice);
+        }
+    }
+}
