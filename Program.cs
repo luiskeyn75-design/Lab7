1059be9072176dc1e97f25aaa5563587a58b173b
DN <luiskeyn75@gmail.com>
2026-02-18 20:45:14 +0200

    Task1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab7Task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Action> actions = new List<Action>();
+            for(int i = 1; i <= 5; i++)
+            {
+                int temp = i;
+                actions.Add(() => Console.WriteLine(temp));
+            }
+
+            foreach(var action in actions)
+            {
+                action();
+            }
+        }
+    }
+}
