c2d068f6d33ac4ccd72c2c96d97077830f96dfda
DN <luiskeyn75@gmail.com>
2026-02-18 20:47:32 +0200

    Task2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab7Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankTerminal terminal = new BankTerminal();
+            terminal.OnMoneyWithdraw += (money) => Console.WriteLine($"Знято:{money} ");
+            terminal.MoneyWithdraw(150);
+          
+        }
+    }
+
+    public class BankTerminal
+    {
+        public event Action<int> OnMoneyWithdraw;
+        public void MoneyWithdraw(int amount)
+        {
+            OnMoneyWithdraw?.Invoke(amount);
+
+        }
+        
+    }
+}
